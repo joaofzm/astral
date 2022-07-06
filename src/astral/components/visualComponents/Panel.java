e0d7ad59e521eb0c3c307d6f6152114bd48eadb8
joao <joao.fzm@hotmail.com>
2022-07-05 21:17:31 -0300

    Add more overloaded add methods to Panel class

--- src/astral/components/visualComponents/Panel.java
+++ src/astral/components/visualComponents/Panel.java
@@ -5,6 +5,7 @@ import javax.swing.JPanel;
 
 import astral.component.Component;
 import astral.config.Config;
+import astral.test.panels.MenuPanel;
 
 
 public class Panel implements Component {
@@ -22,6 +23,16 @@ public class Panel implements Component {
 	
 	public void add(Component c) {
 		panel.add(c.getJComponent());
+	}
+	
+	public void add(TextButton c, MenuPanel p) {
+		panel.add(c.getJComponent());
+		c.getJComponent().addActionListener(p);
+	}
+	
+	public void add(CenteredTextButton c, MenuPanel p) {
+		panel.add(c.getJComponent());
+		c.getJComponent().addActionListener(p);
 	}
 
 }
--- src/astral/test/panels/MenuPanel.java
+++ src/astral/test/panels/MenuPanel.java
@@ -9,7 +9,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import astral.components.visualComponents.CenteredTextButton;
-import astral.components.visualComponents.Label;
 import astral.components.visualComponents.Panel;
 import astral.components.visualComponents.TextButton;
 import astral.config.Config;
@@ -17,42 +16,40 @@ import astral.config.Config;
 
 public class MenuPanel implements ActionListener {
 
-	private Panel panel;
+	private Panel panel = new Panel(1920,1080);
 	public Panel getPanel() {
 		return panel;
 	}
 	
+	private JFrame frame;
 	private JLabel bg;
 	
 	private TextButton exitButton;
-	
 	private CenteredTextButton anotherButton;
 
-	private JFrame frame;
 	
 	public MenuPanel(JFrame frame) {
 		
 		this.frame=frame;
 
-		panel = new Panel(1920,1080);
-		
 		exitButton = new TextButton(892, 850, 136, 62,"Exit",62, 255,255,255,0,0,0);
-		exitButton.getJComponent().addActionListener(this);
-		panel.add(exitButton);
+		panel.add(exitButton,this);
 		
-		anotherButton = new CenteredTextButton(250, 60, 62,"Ot",62, 255,255,255,0,0,0);
-		anotherButton.getJComponent().addActionListener(this);
-		panel.add(anotherButton);
+		anotherButton = new CenteredTextButton(250, 140, 62,"TEST",62, 255,255,255,0,0,0);
+		panel.add(anotherButton,this);
 		
 		bg = new JLabel();
 		
-		ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("Backgrounds/bg1920x1080.png"));
-		bg.setSize(1920,1080);
-		if (Config.res==2) {
+		ImageIcon icon = null;
+		if (Config.res == 1) {
+			icon = new ImageIcon(getClass().getClassLoader().getResource("Backgrounds/bg1920x1080.png"));
+			bg.setSize(1920,1080);
+		} else if (Config.res==2) {
 			icon = new ImageIcon(getClass().getClassLoader().getResource("Backgrounds/bg1280x720.png"));
 			bg.setSize(1280,720);
 		}
 		bg.setIcon(icon);
+		
 		panel.getJComponent().add(bg);
 
 	}
